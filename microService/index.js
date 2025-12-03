04950316f287b8ddd6dbb0e00855127110eb0226
Ludovic POTRICH <ludovic.potrich@macbookpro.home>
2025-12-03 14:38:14 +0100

    get/prenom

--- microService/index.js
+++ microService/index.js
@@ -12,10 +12,26 @@ app.use(cors());
 
 //installer un service get
 //maping entre url et code
+//mon test
+/*
 app.get("/", function (req, res) {
     console.log("Bien reçu");
-    res.send("Bien reçu");
+    //on envoie un retour simple
+    res.json("Bien reçu");
+    res.end();
 });
+*/
+app.get('/:prenom', function (req, res) {
+    console.log(req.params)
+    console.log(req.body)
+    console.log("Bonjour à toi ", req.params.prenom);
+    res.statusCode = '200'
+    res.json({
+        message: "Bonjour à toi " + req.params.prenom
+    });
+    res.end()
+})
+
 
 
 app.listen(8000, function () {
