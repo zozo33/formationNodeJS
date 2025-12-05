1f085a885c92ea134f16582054a9e0e499028946
Ludovic POTRICH <ludovic.potrich@macbookpro.home>
2025-12-05 11:09:03 +0100

    first vue avec vuejs et app2 pour un appel à un service rest

--- vueJS/lab-vuejs/src/App.vue
+++ vueJS/lab-vuejs/src/App.vue
@@ -1,11 +1,30 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
+  <h1>{{ titre }}</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <p>Compteur : {{ compteur }}</p>
+  <button @click="compter">Compter</button>
 </template>
+
+<script>
+export default {
+    name: "App",
+    data() {
+        return {
+            compteur: 10,
+            titre: "Mon titre"
+        }
+    },
+    methods: {
+        compter() {
+            this.compteur++
+        }
+    }
+}
+</script>
 
 <style scoped></style>
---
+++ vueJS/lab-vuejs/src/App2.vue
@@ -0,0 +1,39 @@
+<script setup>
+</script>
+
+<!--faire tourner avec la commande node dbservicePostgres.js -->
+
+<template>
+  <div>
+    <table>
+      <button @click='getdata'>Lister les formations</button>
+      <tr v-for="f in formations">
+        <td>{{f.id}}</td><td>{{ f.libelle }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+ <script>
+export default {
+    data () {
+      return { 
+        compteur : 0,
+        formations : []
+      }
+    },
+    methods : {
+      getdata ()
+      {
+          fetch ("http://localhost:8000/").then (d => d.json()).then(
+              formations => {
+                  this.formations = formations;
+              }
+          )
+      }
+    }
+  };
+</script>
+
+<style scoped>
+</style>
